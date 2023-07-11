0b250dd515c6b5bc591c5b844b32dd026062bea4
meanish <anishgshrestha@gmail.com>
2023-07-11 15:51:40 +0545

    userupdate

--- backend/routes/userRegisterRouter.js
+++ backend/routes/userRegisterRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 
-const UserRegisterController = require("../controllers/UserRegisterController")
+const UserRegisterController = require("../controllers/userRegisterController")
 
 
 
