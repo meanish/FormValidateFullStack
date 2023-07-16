e6a92aa8f500ac5309f6be141d7ee997c9988a5e
meanish <anishgshrestha@gmail.com>
2023-07-17 00:15:33 +0545

    mongolink

--- backend/src/dB/conn.js
+++ backend/src/dB/conn.js
@@ -3,10 +3,11 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    dbName: "blog", // Specify the database name
 })
     .then(() => {
         console.log("connected to the db")
     })
     .catch(() => {
-        console.log("error in connecting with dB");
+        console.log("error in connecting with dataBase");
     })
