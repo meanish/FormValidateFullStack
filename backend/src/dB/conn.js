81f19ebe65cca975e367a04a59d0595d5e9019f7
meanish <anishgshrestha@gmail.com>
2023-08-08 13:40:18 +0545

    changed

--- backend/src/dB/conn.js
+++ backend/src/dB/conn.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    dbName: "blog", // Specify the database name
-})
-    .then(() => {
-        console.log("connected to the db")
-    })
-    .catch(() => {
-        console.log("error in connecting with dataBase");
-    })
+  })
+  .then(() => {
+    console.log("connected to the db");
+  })
+  .catch(() => {
+    console.log("error in connecting with dataBase");
+  });
